d72d6a81de9bff4dbe4d823d47c6f935a039972c
gustavo-bgi <gustavobgi1411@gmail.com>
2026-02-06 08:07:52 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -8,8 +8,8 @@
 // 3. Vá em Settings > API
 // 4. Copie a URL e a anon key
 
-const SUPABASE_URL = 'SEU_SUPABASE_URL_AQUI'; // Ex: https://xxxxx.supabase.co
-const SUPABASE_ANON_KEY = 'SUA_SUPABASE_ANON_KEY_AQUI';
+const SUPABASE_URL = 'https://mlftdhglevxgpfeyjtnl.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sZnRkaGdsZXZ4Z3BmZXlqdG5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzNTMyNjgsImV4cCI6MjA4NTkyOTI2OH0.SmL5tUpE_zlqzyPlcQqTys7CTzhNnJGjRfWLQqN75lA';
 
 // Inicializar cliente Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
